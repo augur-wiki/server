0973712348a4557a5f456b4581ffd2b328ab2f6c
Anish-Agnihotri <contact@anishagnihotri.com>
2019-03-13 22:08:16 -0400

    Adding server refresh

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+dotenv_1.default.config();
+const port = process.env.SERVER_PORT;
 const app = express_1.default();
-const port = 8084; // default port to listen
 // define a route handler for the default home page
 app.get("/", (req, res) => {
     res.send("Hello world!");
